4f8316ce37d5ba863e64b6babe6bd45634b4f1df
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-12-01 13:43:05 -0500

    ajout fonction autocomplete

--- server.js
+++ server.js
@@ -16,20 +16,52 @@ mongoose.connect(
  .catch(err => console.log(err));
 
 
-// CRUD API
+// CRUD API //
+
+// GET
 app.get("/releases", async (req, res) => {
-  const q = req.query.q || "";
-  const releases = await Release.find({ 
-    game: { $regex: q, $options: "i" } })
-    .limit(10);
+  const releases = await Release.find().limit(100);
   res.json(releases);
 });
 
+app.get("/releases/autocomplete", async (req, res) => {
+  const q = req.query.q;
+  if (!q) return res.json([]);
+
+  const results = await Release.find(
+    { game: { $regex: q, $options: "i" } },
+    { game: 1 }      // on renvoie seulement game
+  ).limit(10);
+
+  res.json(results);
+});
+
+app.get("/releases/first10", async (req, res) => {
+  const list = await Release.find().limit(10);
+  res.json(list);
+});
+
+app.get("/releases/search", async (req, res) => {
+  const q = req.query.q || "";
+  const results = await Release.find({
+    game: { $regex: q, $options: "i" }
+  }).limit(30); // évite les milliers de résultats
+
+  res.json(results);
+});
+
+app.get("/releases/:id", async (req, res) => {
+  const item = await Release.findById(req.params.id);
+  res.json(item);
+});
+
+// POST
 app.post("/releases", async (req, res) => {
   const newRelease = await Release.create(req.body);
   res.json(newRelease);
 });
 
+// PUT
 app.put("/releases/:id", async (req, res) => {
   const updated = await Release.findByIdAndUpdate(
     req.params.id,
@@ -39,10 +71,12 @@ app.put("/releases/:id", async (req, res) => {
   res.json(updated);
 });
 
+// DELETE
 app.delete("/releases/:id", async (req, res) => {
   await Release.findByIdAndDelete(req.params.id);
   res.json({ message: "Supprimé" });
 });
+
 
 
 app.listen(3000, () => console.log("Serveur sur http://localhost:3000"));
